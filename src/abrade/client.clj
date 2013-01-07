a4a1ab60a3955d83c09e446b806891b608d626e2
James Reeves <jreeves@weavejester.com>
2013-01-07 18:45:34 +0000

    Added tag and attrs functions

--- src/abrade/client.clj
+++ src/abrade/client.clj
@@ -14,12 +14,25 @@
   [browser url]
   (.getPage browser url))
 
-(defn css
-  "Get elements in a page by CSS selector."
-  [page selector]
-  (seq (.querySelectorAll page selector)))
+(defn tag
+  "Get the tag name of an element."
+  [element]
+  (.getTagName element))
+
+(defn attrs
+  "Get the attributes of an element."
+  [element]
+  (->> (.getAttributes element)
+       (map #(.getValue %))
+       (map #(vector (.getName %) (.getValue %)))
+       (into {})))
 
 (defn text
   "Return the text in an element."
   [element]
   (.asText element))
+
+(defn css
+  "Get elements in a page by CSS selector."
+  [page selector]
+  (seq (.querySelectorAll page selector)))
