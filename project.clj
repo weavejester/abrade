15e139b0f8e8cdd2bebb30a9910d1815584ca2c5
James Reeves <jreeves@weavejester.com>
2013-01-07 18:56:46 +0000

    Updated project description and URL

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject abrade "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A Clojure library for scraping websites"
+  :url "https://github.com/weavejester/abrade"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.2.1"]
