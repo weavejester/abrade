a1d9a926de2462d0677991ccf41d638092d0f47d
James Reeves <jreeves@weavejester.com>
2013-01-07 18:00:34 +0000

    Added basic client functions

--- project.clj
+++ project.clj
@@ -3,4 +3,6 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]])
+  :dependencies [[org.clojure/clojure "1.2.1"]
+                 [org.clojure/data.zip "0.1.1"]
+                 [net.sourceforge.htmlunit/htmlunit "2.11"]])
--- src/abrade/client.clj
+++ src/abrade/client.clj
@@ -1,6 +1,25 @@
-(ns abrade.client)
+(ns abrade.client
+  (:import [com.gargoylesoftware.htmlunit
+            BrowserVersion WebClient]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defmulti browser
+  "Return a browser object of the specified type."
+  identity)
+
+(defmethod browser :firefox [_]
+  (WebClient. (BrowserVersion/FIREFOX_3_6)))
+
+(defn open
+  "Open a page in the supplied browser."
+  [browser url]
+  (.getPage browser url))
+
+(defn css
+  "Get elements in a page by CSS selector."
+  [page selector]
+  (seq (.querySelectorAll page selector)))
+
+(defn text
+  "Return the text in an element."
+  [element]
+  (.asText element))
